df21d5e157d51d9ca7b688e79b61d415eba67568
eduardo-brigato <edubrigato@gmail.com>
2024-08-05 19:16:33 -0300

    feat: started project

---
+++ src/main/java/br/com/petcenter/petcenter/PetCenterApplication.java
@@ -0,0 +1,13 @@
+package br.com.petcenter.petcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/petcenter/petcenter/PetCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petcenter.petcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
